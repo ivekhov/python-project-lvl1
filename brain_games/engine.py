21b00b05e24c451567a108c69cdc736b4b61c9a7
ivan <ivan@1s-MacBook-Pro.local>
2020-08-19 12:46:39 +0300

    Cleaned some typo.

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,26 +10,23 @@ def play_game(game):
     Create boilerplate for game.
 
     Args:
-        greet: welcome phrase per particuar game.
-        get_question_and_answer: function.
+        game: function with particular game.
     """
     print('Welcome to the Brain Games!')
     print(game.DESCRIPTION)
     name = prompt.string(prompt='\nMay I have your name? ')
     print('Hello, {}!'.format(name))
     print()
-    counter = ATTEMPTS
-    while counter > 0:
+    for attempt in range(0, ATTEMPTS):
         question, correct = game.get_question_and_answer()
         print('Question: {}'.format(question))
         answer = prompt.string(prompt='Your answer: ')
         if answer == correct:
             print('Correct!')
-            counter = counter - 1
         else:
             print("'{}' is wrong answer ;(.".format(answer), end=' ')
             print("Correct answer was {}".format(correct))
             print("Let's try again, {}!".format(name))
             break
-        if counter == 0:
+        if attempt == 2:
             print('Congratulations, {}!\n'.format(name))
